6e7062bc6b1e8a080665e015521604ae7c9d2260
DennisTarbiat <dennis.tarbiat@gmail.com>
2022-09-23 15:12:25 +0200

    added complete code to functions

---
+++ objects/group-1/app.js
@@ -0,0 +1,10 @@
+const {setupGroups} = require("./index.js");
+const {getStudents} = require("./index.js");
+const {getStudent} = require("./index.js");
+const {getGroup} = require("./index.js");
+const {assignGroup} = require("./index.js");
+const students = require("./groups")
+
+
+
+console.log(setupGroups());
--- objects/group-1/index.js
+++ objects/group-1/index.js
@@ -1,8 +1,6 @@
-
+require('dotenv').config()
  const students = require("./groups")
- // const students = require("../data/groups")
  
- const allStudents = students.all // read students from imported module
  let ambitionDefined,
      groupsAssigned
  
@@ -26,7 +24,7 @@
      // ... 
      index++
  
-     if (index >= allStudents.length) {
+     if (index >= students.all.length) {
          return true // remember that it's good practice to have single point of return for a function
      }
      applyAmbition(index)
@@ -34,31 +32,42 @@
  
  const getStudent = (index) => {
      let i = Number(index) 
-     if (isNaN(i) || students.all.length > i) i = 0 // TODO: fix this. What happens when we don't get good input?
+     if (isNaN(i) || students.all.length < i){
+        console.log('Invalid Input, Index Set To 0');
+        i = 0;
+     }  // TODO: fix this. What happens when we don't get good input?
      else i = index
-     return students.all[i]
+     return console.log(students.all[i])
  }
+
+
  const getStudents = () =>{
      return students.all
  }
  
  const setupGroups = () => {
      
-     ambitionDefined = applyAmbition(0)
-     groupsAssigned = assignGroup(0, 6) // TODO: maybe this need more thought?
- 
-     return students.all   
- }
+    ambitionDefined = applyAmbition(0)
+    function myComparator(a, b) {return a.hoursPerWeek - b.hoursPerWeek }
+    students.all.sort(myComparator)
+    groupsAssigned = assignGroup(0, 6)
+    return students.all
+}
  // TODO: return an array with only the students that belongs to a group with a specific index
+
+
+ let grupp = [];
  const getGroup = (arg) => {
-     return({
-         TODO: "return group " + arg
-         // students: students.all.splice(11,6) // TODO: this works as long as the array of students is sorted by group. Also the size 
-     })
+    for(let i = 0; i < students.all.length; i++){
+        if(students.all[i].group == arg){
+            grupp.push(students.all[i]);
+        }  
+    } return grupp;
+    
  }
  
- exports._setupGroups = setupGroups
- exports._getStudents = getStudents
- exports._getStudent = getStudent
- exports._getGroup = getGroup
- exports._getStudent = getStudent
+ exports.setupGroups = setupGroups
+ exports.getStudents = getStudents
+ exports.getStudent = getStudent
+ exports.getGroup = getGroup
+ exports.assignGroup = assignGroup
